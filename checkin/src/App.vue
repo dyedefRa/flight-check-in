f6a18569f457787528302b7e1f4e771b88301eeb
Merve Erdol <merve-uygun@windowslive.com>
2018-07-05 23:58:00 +0300

    Add welcome page

--- checkin/src/App.vue
+++ checkin/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <img src="./assets/airline.png">
     <router-view/>
   </div>
 </template>
@@ -19,5 +19,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+img {
+  padding-bottom : 30px
 }
 </style>
